b505c6149bbd66b832b74cb242c871fa6f221eb0
marcio Lopez <ciomar2010@gmail.com>
2019-06-07 18:28:42 -0300

    agrego atributo de tipo objeto Mascota en la clase User, necesaria para luego hacer el abm

--- src/java/aplicacion/modelo/dominio/User.java
+++ src/java/aplicacion/modelo/dominio/User.java
@@ -3,126 +3,113 @@ package aplicacion.modelo.dominio;
 import java.util.Date;
 
 /**
+ * clase generica para definir un CLIENTE que en este caso sera el dueño de la mascota
  *
- * @author clavedesol
- */
-/**
- * clase generica para definir un Usuario ya sea administrador, administrativo o
- * cliente
+ * @author GRUPO10
  */
 public class User {
 
     /**
      * atributo de tipo String identificando el documento nacional de identidad
      */
-    private String dni;
+    public String dni;
     /**
      * atributo de tipo String que identifica el apellido del User
      */
-    private String apellido;
+    public String apellido;
     /**
      * atributo de tipo String que identifica el nombre del User
      */
-    private String nombre;
+    public String nombre;
     /**
      * atributo de tipo Date con la fecha de nacimiento del User
      */
-    private Date fechaNac;
+    public Date fechaNac;
     /**
      * atributo de tipo estado que indica si un User esta activo, incluido en el
      * sistema etc
      */
-    private boolean estado;
 
+    public boolean estado;
+    /**
+     * atributo de tipo objeto Mascota que indica la relacion entre un User y su
+     * mascota
+     */
+    public Mascota mascota;
+
+    /**
+     * constructor por defecto de la clase
+     */
     public User() {
+
     }
 
     /**
-     * constructor parametrizado de la clase User
+     * constructor parametrizado de la clase
      *
-     * @param dni documento nacional de identidad
-     * @param apellido apellido del User
-     * @param nombre nombre del User
-     * @param fechaNac fecha de nacimiento del User
-     * @param estado estado true or false del User
+     * @param dni el documento nacional de identidad
+     * @param apellido el apellido del User
+     * @param nombre el nombre del User
+     * @param fechaNac la fecha de Nacimiento del User
+     * @param estado el estado del User dentro del Sistema
+     * @param mascota el objeto Mascota
      */
-    public User(String dni, String apellido, String nombre, Date fechaNac, boolean estado) {
+    public User(String dni, String apellido, String nombre, Date fechaNac, boolean estado, Mascota mascota) {
         this.dni = dni;
         this.apellido = apellido;
         this.nombre = nombre;
         this.fechaNac = fechaNac;
         this.estado = estado;
+        this.mascota = mascota;
     }
-//SECCION DE GETTERS Y SETTERS
 
-    /**
-     * @return the dni
-     */
+    // SECCION DE GETTERS Y SETTERS
     public String getDni() {
         return dni;
     }
 
-    /**
-     * @param dni the dni to set
-     */
     public void setDni(String dni) {
         this.dni = dni;
     }
 
-    /**
-     * @return the apellido
-     */
     public String getApellido() {
         return apellido;
     }
 
-    /**
-     * @param apellido the apellido to set
-     */
     public void setApellido(String apellido) {
         this.apellido = apellido;
     }
 
-    /**
-     * @return the nombre
-     */
     public String getNombre() {
         return nombre;
     }
 
-    /**
-     * @param nombre the nombre to set
-     */
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
 
-    /**
-     * @return the fechaNac
-     */
     public Date getFechaNac() {
         return fechaNac;
     }
 
-    /**
-     * @param fechaNac the fechaNac to set
-     */
     public void setFechaNac(Date fechaNac) {
         this.fechaNac = fechaNac;
     }
 
-    /**
-     * @return the estado
-     */
     public boolean isEstado() {
         return estado;
     }
 
-    /**
-     * @param estado the estado to set
-     */
     public void setEstado(boolean estado) {
         this.estado = estado;
+    }
+
+    public Mascota getMascota() {
+        return mascota;
+    }
+
+    public void setMascota(Mascota mascota) {
+        this.mascota = mascota;
     }
 
 }
