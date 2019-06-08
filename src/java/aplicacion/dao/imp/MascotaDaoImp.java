52b8a5603a370ae9eaf9eb1fd1d08a53114231c4
Jose8922 <vico_joseluis@hotmail.com>
2019-06-08 20:32:10 -0300

    Arreglo del Dao Clase MascotaDaoImp creada

---
+++ src/java/aplicacion/dao/imp/MascotaDaoImp.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aplicacion.dao.imp;
+
+import aplicacion.Dao.IMascotaDao;
+import aplicacion.modelo.dominio.Mascota;
+import java.util.List;
+
+/**
+ *
+ * @author GrupoDiez
+ */
+public class MascotaDaoImp implements IMascotaDao{
+
+    @Override
+    public List<Mascota> obtenerTodos() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void crear(Mascota mascota) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void modificar(Mascota mascota) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void eliminar(Mascota mascota) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Mascota obtenerPaciente(int historiaclinica) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
