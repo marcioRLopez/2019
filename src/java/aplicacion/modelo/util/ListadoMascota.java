b8d24c28fda2aef580ddf42940db16629b247a8e
ezequielreneclavijo <>
2019-06-08 23:46:04 -0300

    He agregado una clase ListadoMascota con sus respectivos métodos accesores

---
+++ src/java/aplicacion/modelo/util/ListadoMascota.java
@@ -0,0 +1,30 @@
+package aplicacion.modelo.util;
+
+import aplicacion.modelo.dominio.Mascota;
+import java.util.List;
+
+/**
+ *
+ * @author EZEQUIEL
+ */
+public class ListadoMascota {
+
+    //Lista de mascotas
+    private static List<Mascota> tablaMascotas;
+
+    //Métodos accesores get y set
+    /**
+     * @return the tablaMascotas
+     */
+    public static List<Mascota> getTablaMascotas() {
+        return tablaMascotas;
+    }
+
+    /**
+     * @param aTablaMascotas the tablaMascotas to set
+     */
+    public static void setTablaMascotas(List<Mascota> aTablaMascotas) {
+        tablaMascotas = aTablaMascotas;
+    }
+
+}
