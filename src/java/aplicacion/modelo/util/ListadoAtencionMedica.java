b724296a6385911c6133ac0b4a667bcda107c911
ezequielreneclavijo <>
2019-06-09 00:38:33 -0300

    He agregado una clase ListadoAtencionMedica y atributos Lista con su respectivos métodos accesores

---
+++ src/java/aplicacion/modelo/util/ListadoAtencionMedica.java
@@ -0,0 +1,33 @@
+package aplicacion.modelo.util;
+
+import aplicacion.modelo.dominio.AtencionMedica;
+import java.util.List;
+
+/**
+ *
+ * @author EZEQUIEL
+ */
+public class ListadoAtencionMedica {
+
+    //Lista para guardar atenciones médicas
+    private static List<AtencionMedica> tablaAtencionesMedicas;
+
+    public ListadoAtencionMedica() {
+    }
+
+    //Métodos accesores
+    /**
+     * @return the tablaAtencionesMedicas
+     */
+    public static List<AtencionMedica> getTablaAtencionesMedicas() {
+        return tablaAtencionesMedicas;
+    }
+
+    /**
+     * @param aTablaAtencionesMedicas the tablaAtencionesMedicas to set
+     */
+    public static void setTablaAtencionesMedicas(List<AtencionMedica> aTablaAtencionesMedicas) {
+        tablaAtencionesMedicas = aTablaAtencionesMedicas;
+    }
+
+}
