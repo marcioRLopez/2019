2c7849676e081f4228bc18f970b7a129315fbf26
Anachuri Alejandro <Alejandro@DESKTOP-RGJN1CC>
2019-06-10 04:01:43 -0300

    - Codificacion del codigo de la Clase Producto. 	Atrbutos. 	Constructores. 	GETTERS Y SETTERS.

--- src/java/aplicacion/modelo/dominio/Producto.java
+++ src/java/aplicacion/modelo/dominio/Producto.java
@@ -5,10 +5,59 @@
  */
 package aplicacion.modelo.dominio;
 
+import java.io.Serializable;
+
 /**
  *
  * @author Grupo10
  */
-public class Producto {
+public class Producto implements Serializable{
+    private int codigo;
+    private String nombre;
+    private float precio;
+    private String descripcion;
+
+    public Producto() {
+    }
+
+    public Producto(int codigo, String nombre, float precio, String descripcion) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.precio = precio;
+        this.descripcion = descripcion;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public float getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(float precio) {
+        this.precio = precio;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+    
     
 }
