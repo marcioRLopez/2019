c0065ae0b4337c11001ecd331721ee53efe009fc
Jose8922 <vico_joseluis@hotmail.com>
2019-06-11 04:01:19 -0300

    agrego atributos Medicamentos

--- src/java/aplicacion/modelo/dominio/Medicamento.java
+++ src/java/aplicacion/modelo/dominio/Medicamento.java
@@ -5,10 +5,16 @@
  */
 package aplicacion.modelo.dominio;
 
+import java.io.Serializable;
+
 /**
  *
  * @author Grupo10
  */
-public class Medicamento {
+public class Medicamento implements Serializable {
+    
+    public String nombre;
+    public String fecha_vencimiento;
+    
     
 }
