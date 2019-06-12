3ddf0221ac0e012276bb358e0de746ce0f8aaa24
Jose8922 <vico_joseluis@hotmail.com>
2019-06-12 01:11:26 -0300

    Creacion de la clase Historia Clinica con sus atributos los geters y seters asigno atributos a la clase vacuna con sus geters  y seters

--- src/java/aplicacion/modelo/dominio/Vacuna.java
+++ src/java/aplicacion/modelo/dominio/Vacuna.java
@@ -5,10 +5,47 @@
  */
 package aplicacion.modelo.dominio;
 
+import java.io.Serializable;
+
 /**
  *
  * @author Grupo10
  */
-public class Vacuna {
+public class Vacuna implements Serializable {
+
+    public Vacuna(String nombre, String tipo_vacuna) {
+        this.nombre = nombre;
+        this.tipo_vacuna = tipo_vacuna;
+    }
     
+    private String nombre;
+    private String tipo_vacuna;
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the tipo_vacuna
+     */
+    public String getTipo_vacuna() {
+        return tipo_vacuna;
+    }
+
+    /**
+     * @param tipo_vacuna the tipo_vacuna to set
+     */
+    public void setTipo_vacuna(String tipo_vacuna) {
+        this.tipo_vacuna = tipo_vacuna;
+    }
 }
