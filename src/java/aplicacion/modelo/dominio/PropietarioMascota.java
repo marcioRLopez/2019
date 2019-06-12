416a124026bf69cb906691c68b11c6bcb4310cb6
Anachuri Bruno Alejandro <Alejandro@DESKTOP-RGJN1CC.lan>
2019-06-11 23:38:48 -0300

    Modificacion de la clase PropietarioMascota. - Agrege nuevos atributos (dni, nombre, apellido, direccion, telefono, correo) a la clase. - Agrege metodos accesores por defecto y parametrizado a la clase. - Cree los metodos getters y setters

--- src/java/aplicacion/modelo/dominio/PropietarioMascota.java
+++ src/java/aplicacion/modelo/dominio/PropietarioMascota.java
@@ -1,110 +1,99 @@
 package aplicacion.modelo.dominio;
 
 import java.io.Serializable;
-import java.util.Date;
 
 /**
- * clase CLIENTE que en este caso sera el dueño de la
+ * clase PropietarioMascota que en este caso sera el dueño de la
  * mascota
  *
  * @author GRUPO10
  */
 public class PropietarioMascota implements Serializable {
-
+    
     /**
-     * atributo de tipo estado que indica si un PropietarioMascota esta
- activo, incluido en el sistema etc
+     * Atributos de la clase de propietario de la mascota
      */
-    private boolean estado;
-    /**
-     * Domicilio del propietario
-     */
+    private long dni;
+    private String nombre;
+    private String apellido;
     private String direccion;
-    /**
-     * Número telefonico del propietario
-     */
-    private String telefono;
+    private long telefono;
+    private String correo;
 
     /**
-     * constructor por defecto de la clase
+     * Constructor por defecto
      */
     public PropietarioMascota() {
-
     }
-
+    
     /**
-     * constructor parametrizado
-     *
-     * @param estado el estado del propietario dentro del Sistema
-     * @param direccion el objeto Mascota
-     * @param telefono el objeto Mascota
+     * Constructor Parametrizado
+     * @param dni
+     * @param nombre
+     * @param apellido
+     * @param direccion
+     * @param telefono
+     * @param correo 
      */
-    public PropietarioMascota(boolean estado, String direccion, String telefono) {
-        this.estado = estado;
+    public PropietarioMascota(long dni, String nombre, String apellido, String direccion, long telefono, String correo) {
+        this.dni = dni;
+        this.nombre = nombre;
+        this.apellido = apellido;
         this.direccion = direccion;
         this.telefono = telefono;
+        this.correo = correo;
+    }
+    
+    // SECCION DE GETTER Y SETTER
+
+    public String getCorreo() {
+        return correo;
     }
 
-    /**
-     * Constructor con atributos heredados de la clase persona
-     *
-     * @param estado
-     * @param direccion el estado del User dentro del Sistema
-     * @param telefono el objeto Mascota
-     * @param nombre el nombre del dueño dentro del Sistema
-     * @param apellido del propietario
-     * @param fechaDeNacimiento la fecha de nacimiento del dueño dentro del
-     * Sistema
-     * @param dni el Documento Nacionel de Identidad del dueño de Mascota
-     */
-    public PropietarioMascota(boolean estado, String direccion, String telefono, String nombre, String apellido, Date fechaDeNacimiento, String dni) {
-        
-        this.estado = estado;
-        this.direccion = direccion;
-        this.telefono = telefono;
+    public void setCorreo(String correo) {
+        this.correo = correo;
     }
 
-// SECCION DE GETTERS Y SETTERS 
-    /**
-     * @return the estado
-     */
-    public boolean isEstado() {
-        return estado;
+    public long getDni() {
+        return dni;
     }
 
-    /**
-     * @param estado the estado to set
-     */
-    public void setEstado(boolean estado) {
-        this.estado = estado;
+    public void setDni(long dni) {
+        this.dni = dni;
     }
 
-    /**
-     * @return the direccion
-     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
     public String getDireccion() {
         return direccion;
     }
 
-    /**
-     * @param direccion the direccion to set
-     */
     public void setDireccion(String direccion) {
         this.direccion = direccion;
     }
 
-    /**
-     * @return the telefono
-     */
-    public String getTelefono() {
+    public long getTelefono() {
         return telefono;
     }
 
-    /**
-     * @param telefono the telefono to set
-     */
-    public void setTelefono(String telefono) {
+    public void setTelefono(long telefono) {
         this.telefono = telefono;
     }
+    
+    
 
 }
