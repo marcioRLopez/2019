214eac89be5f552cccae42ec59bc7671bb7bf856
Jose8922 <vico_joseluis@hotmail.com>
2019-06-13 01:11:24 -0300

    Creacion de la clase IDoctorDao En Doctor.hbm.xml faltaria agregar property de las atributos

---
+++ src/java/aplicacion/hibernate/dao/IDoctorDAO.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aplicacion.hibernate.dao;
+
+import aplicacion.modelo.dominio.Doctor;
+
+/**
+ *
+ * @author Grupo10
+ */
+public interface IDoctorDAO {
+    void create(Doctor doctor);
+}
