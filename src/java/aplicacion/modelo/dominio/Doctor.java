e01a344ae232624df1c0baecdc62a602fd3b671a
Jose8922 <vico_joseluis@hotmail.com>
2019-06-12 22:37:41 -0300

    agrego atributos a la clase Doctor idCodigo y codigo elimino la clase UsuarioDaoImp y ListadoUsuario

--- src/java/aplicacion/modelo/dominio/Doctor.java
+++ src/java/aplicacion/modelo/dominio/Doctor.java
@@ -6,24 +6,24 @@ import java.io.Serializable;
  * AUTOR GRUPO10 clase que nos indica el Doctor y todos sus atributos
  */
 public class Doctor implements Serializable {
-
+    private Integer idCodigo;
+    private Boolean estado;    
      /**
      * atributo que indica el nombre de Doctor utilizado en el sistema
      */
-    public String nombreUsuario;
+    private String nombreUsuario;
     /**
      * atributo que indica el password de ingreso al sistema del Doctor
      */
-    public String password;
+    private String password;
     /**
      * atributo que nos da la direccion del e-mail del Doctor
      */
-    public String correoElectronico;
+    private String correoElectronico;
     /**
      * atributo de tipo objeto de nombre PropietarioDeMascota propietarioDeMascota
      */
 
-    
 
     /**
      * cosntructor por defecto de la clase
@@ -39,7 +39,9 @@ public class Doctor implements Serializable {
      * @param password password o contraseña del Usuario dentro del Sistema
      * @param correoElectronico direccion del E-mail del Usuario
      */
-    public Doctor(String nombreUsuario, String password, String correoElectronico) {
+    public Doctor(Integer idCodigo, Boolean estado, String nombreUsuario, String password, String correoElectronico) {
+        this.idCodigo = idCodigo;
+        this.estado = estado;
         this.nombreUsuario = nombreUsuario;
         this.password = password;
         this.correoElectronico = correoElectronico;
@@ -69,5 +71,19 @@ public class Doctor implements Serializable {
 
     public void setCorreoElectronico(String correoElectronico) {
         this.correoElectronico = correoElectronico;
+    }
+    public Integer getIdCodigo() {
+        return idCodigo;
+    }
+
+    public void setIdCodigo(Integer idCodigo) {
+        this.idCodigo = idCodigo;
+    }
+    public Boolean getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Boolean estado) {
+        this.estado = estado;
     }
 }
