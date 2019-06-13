80b65577b87cb5c59eb069139531afdf04a88ea9
ezequielreneclavijo <>
2019-06-12 21:13:14 -0300

    Modificando los beans y beansForms

--- src/java/aplicacion/controlador/beans/UsuarioBean.java
+++ src/java/aplicacion/controlador/beans/UsuarioBean.java
@@ -1,63 +1,20 @@
 package aplicacion.controlador.beans;
 
-//import aplicacion.dao.IUsuarioDao;
-//import aplicacion.dao.imp.UsuarioDaoImp;
-//import aplicacion.modelo.dominio.PropietarioDeMascota;
-
-import aplicacion.modelo.dominio.Doctor;
-import javax.annotation.ManagedBean;
+import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
-
 
 /**
  *
- * @author Grupo10
+ * @author EZEQUIEL
  */
 @ManagedBean
 @RequestScoped
 public class UsuarioBean {
 
     /**
-     * atributo de tipo objeto Doctor
+     * Creates a new instance of UsuarioBean
      */
-    private Doctor usuario;
-
-    /**
-     * crea un nueva instancia de UsuarioBean
-     */
-    
-    
-         // error!!! Metodo comentado por atributo(PropietarioDeMascota) extraido de la clase Doctor
-         // Se comentaron algunas clases comentadas
-    //****************************************************************************
-    /**
     public UsuarioBean() {
-        if (usuario != null) {
-            String nombreUsuario = usuario.getNombreUsuario();
-            IUsuarioDao usuarioDao = new UsuarioDaoImp();
-            usuario = usuarioDao.obtenerUsuario(nombreUsuario);
-            if (usuario.getPropietarioDeMascota() == null) {
-                usuario.setPropietarioDeMascota(new PropietarioDeMascota());
-            }
-        }
-    }**/
-    //****************************************************************************
-
-    
-    
-    
-    /**
-     * @return the usuario
-     */
-    public Doctor getUsuario() {
-        return usuario;
     }
-
-    /**
-     * @param usuario the usuario to set
-     */
-    public void setUsuario(Doctor usuario) {
-        this.usuario = usuario;
-    }
-
+    
 }
--- src/java/aplicacion/controlador/beans/forms/UsuarioFormBean.java
+++ src/java/aplicacion/controlador/beans/forms/UsuarioFormBean.java
@@ -1,84 +1,20 @@
 package aplicacion.controlador.beans.forms;
 
-import aplicacion.controlador.beans.UsuarioBean;
-//import aplicacion.dao.IUsuarioDao;
-//import aplicacion.dao.imp.UsuarioDaoImp;
-import aplicacion.modelo.util.ListadoUsuario;
-import javax.annotation.ManagedBean;
-//import javax.faces.application.FacesMessage;
-import javax.faces.bean.ManagedProperty;
+import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
-//import javax.faces.context.FacesContext;
 
 /**
  *
- * @author Grupo10
- *
- * Bean Administrativo de la clase
+ * @author EZEQUIEL
  */
 @ManagedBean
 @RequestScoped
 public class UsuarioFormBean {
 
     /**
-     * inyecta la clase bean
+     * Creates a new instance of UsuarioFormBean
      */
-    @ManagedProperty(value = "#{usuarioBean}")
-    /**
-     * atributo de tipo objeto bean
-     */
-    private UsuarioBean usuarioBean;
-
     public UsuarioFormBean() {
     }
-
-    /**
-     * metodo para actualizar los datos del UsuarioBean a su vez actualiza los
- datos de la clase Doctor y Clase PropietarioDeMascota
-     */
     
-    
-         
-            // error!!! Metodo comentado por atributo(PropietarioDeMascota) extraido de la clase Doctor
-            // Se comentaron algunas clases comentadas
-    //****************************************************************************
-    /**
-    public void actualizarDatos() {
-        IUsuarioDao usuarioDao = new UsuarioDaoImp();
-        usuarioBean.getUsuario().getPropietarioDeMascota().setEstado(true);
-        usuarioDao.modificar(usuarioBean.getUsuario());
-        FacesMessage facesmessage = new FacesMessage(FacesMessage.SEVERITY_INFO, "Datos actualizados", "Datos Actualizados");
-        FacesContext.getCurrentInstance().addMessage(null, facesmessage);
-
-    }**/
-    //****************************************************************************
-    
-    
-    
-    
-    
-    /**
-     * lista de usuarios y get de listado de usuarios
-     *
-     * @return regresa el usuario en la lista
-     */
-    public ListadoUsuario getListadoUsuarios() {
-        ListadoUsuario usuario = new ListadoUsuario();
-        return usuario;
-    }
-
-    /**
-     * @return the usuarioBean
-     */
-    public UsuarioBean getUsuarioBean() {
-        return usuarioBean;
-    }
-
-    /**
-     * @param usuarioBean the usuarioBean to set
-     */
-    public void setUsuarioBean(UsuarioBean usuarioBean) {
-        this.usuarioBean = usuarioBean;
-    }
-
 }
