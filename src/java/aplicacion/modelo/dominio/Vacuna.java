2a682665182e743e06377555744cef2c2df3f777
marcio Lopez <ciomar2010@gmail.com>
2019-06-14 09:07:15 -0300

    agrego comentarios necesarios para la clase Vacuna, añado indicaciones en acta de actividades 1. txt

--- src/java/aplicacion/modelo/dominio/Vacuna.java
+++ src/java/aplicacion/modelo/dominio/Vacuna.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package aplicacion.modelo.dominio;
 
 import java.io.Serializable;
@@ -10,16 +6,41 @@ import java.io.Serializable;
 /**
  *
  * @author Grupo10
+ * Clase que nos indica la vacuna aplicada a la mascota
  */
 public class Vacuna implements Serializable {
+    /**
+     * atributo que identifica el codigo de la vacuna, y facilita su identificacion
+     */
     private Integer idVacuna;
+    /**
+     * atributo que identifica la vacuna por su nombre
+     */
     private String nombre;
+    /**
+     * atributo que identifica el nombre de la vacuna
+     */
     private String tipoVacuna;
+    /**
+     * atributo que identificara el estado de la vacuna que en este caso puecde ser
+     * aplicada, no aplicada, disponible no disponible, 
+     * 
+     */
     private Boolean estado;
+    
+    /**
+     * constructor por defecto de la classe
+     */
 
     public Vacuna() {
     }
-
+/**
+ * constructor parametrizado de la clase
+ * @param idVacuna el codigo de la vacuna numero de lote
+ * @param nombre nombre identificatorio de la vacuna
+ * @param tipoVacuna tipo o clase de vacuna 
+ * @param estado  estado aplicada no aplicada, disponible, no disponible
+ */
     public Vacuna(Integer idVacuna, String nombre, String tipoVacuna, Boolean estado) {
         this.idVacuna = idVacuna;
         this.nombre = nombre;
@@ -27,7 +48,7 @@ public class Vacuna implements Serializable {
         this.estado = estado;
     }
 
-    
+    // SECCION DE GETTERS Y SETTERS
     
     /**
      * @return the nombre
