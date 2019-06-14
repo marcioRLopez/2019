beda264065587c366d912aae61ad99fe08dc15e2
marcio Lopez <ciomar2010@gmail.com>
2019-06-14 10:35:58 -0300

    agrego y completo comentarios faltantes a clase TipoDoctor......

--- src/java/aplicacion/modelo/constantes/TipoDoctor.java
+++ src/java/aplicacion/modelo/constantes/TipoDoctor.java
@@ -5,7 +5,7 @@ package aplicacion.modelo.constantes;
  *
  * @author GRUPO10
  * Clase constante de tipo doctor,sera una variable del sistema que mantendra un valor inmutable 
- * durante la vida del programa
+ * durante la vida del programa de atencion veterinaria
  */
 public class TipoDoctor {
     /**
