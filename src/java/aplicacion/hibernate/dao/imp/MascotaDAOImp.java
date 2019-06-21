e7b80f1df4d389e791272ee52b92f7869b4d36d6
Anachuri Bruno Alejandro <>
2019-06-21 00:10:11 -0300

    - Complete los DAOs de la case Mascota utilizando hibernate con Daos Genericos. poseen los metodos crear, eliminar y actualizar

---
+++ src/java/aplicacion/hibernate/dao/imp/MascotaDAOImp.java
@@ -0,0 +1,13 @@
+package aplicacion.hibernate.dao.imp;
+
+import aplicacion.hibernate.dao.IMascotaDAO;
+import aplicacion.modelo.dominio.Mascota;
+import java.io.Serializable;
+
+/**
+ *
+ * @author Alejandro
+ */
+public class MascotaDAOImp extends GenericDAOImp<Mascota, Integer> implements Serializable, IMascotaDAO{
+    
+}
