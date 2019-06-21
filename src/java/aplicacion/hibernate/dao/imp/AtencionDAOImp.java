758d9f4588a72047c07538dfb47cfdbc9b90493e
Anachuri Bruno Alejandro <>
2019-06-21 00:29:52 -0300

    - Complete los DAOs de la case Atencion(medica) utilizando hibernate con Daos Genericos. poseen los metodos crear, eliminar y actualizar

---
+++ src/java/aplicacion/hibernate/dao/imp/AtencionDAOImp.java
@@ -0,0 +1,13 @@
+package aplicacion.hibernate.dao.imp;
+
+import aplicacion.hibernate.dao.IAtencionDAO;
+import aplicacion.modelo.dominio.Atencion;
+import java.io.Serializable;
+
+/**
+ *
+ * @author Alejandro
+ */
+public class AtencionDAOImp extends GenericDAOImp<Atencion, String> implements Serializable, IAtencionDAO{
+    
+}
