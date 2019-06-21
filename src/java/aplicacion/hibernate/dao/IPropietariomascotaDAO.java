5485ed9b782d09ebd67b6666d9347e6d40e7fddb
Anachuri Bruno Alejandro <>
2019-06-21 00:18:05 -0300

    - Complete los DAOs de la case Mascota utilizando hibernate con Daos Genericos. poseen los metodos crear, eliminar y actualizar

--- src/java/aplicacion/hibernate/dao/IPropietariomascotaDAO.java
+++ src/java/aplicacion/hibernate/dao/IPropietariomascotaDAO.java
@@ -7,5 +7,5 @@ import aplicacion.modelo.dominio.Propietariomascota;
  * @author Alejandro
  */
 public interface IPropietariomascotaDAO extends IGenericDAO<Propietariomascota, Integer>{
-    // ESPACIO PARA DEFINIR METODOS ABSTRACTOS ESPECIFICOS DE LA CLASE DOCTOR!!!!!
+    // ESPACIO PARA DEFINIR METODOS ABSTRACTOS ESPECIFICOS DE LA CLASE Propietariomascota!!!!!
 }
