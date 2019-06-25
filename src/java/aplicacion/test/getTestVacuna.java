cac5d03ab4fd219073d2a92282cacde92537b576
ezequielreneclavijo <>
2019-06-25 15:18:47 -0300

    Puse un archivo SQL en la carpeta resource/Base de Datos. Sirve para hacer la prueba de logueo. Por el momento tiene una sola tabla.

---
+++ src/java/aplicacion/test/getTestVacuna.java
@@ -0,0 +1,14 @@
+package aplicacion.test;
+
+import aplicacion.modelo.dominio.Vacuna;
+
+/**
+ *
+ * @author EZEQUIEL
+ */
+public class getTestVacuna {
+
+    public static void main(String[] args) {
+        Vacuna unaVacuna = new Vacuna();
+    }
+}
